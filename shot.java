e4cb0e5602f679f9a1c11406d99bba4aa7d6d880
TaoYang666 <70820944+TaoYang666@users.noreply.github.com>
2020-09-11 08:15:03 +0800

    Add files via upload

---
+++ shot.java
@@ -0,0 +1,45 @@
+import java.until.Scanner
+public class shot
+{
+    public static void main (String [] arg)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print ("Input your x axis of your shoting: ");
+        int x =sc.nextInt();
+        System.out.print ("Input your y axis of your shoting: ");
+        int y =sc.nextInt();
+        Board[i][j]=0;//From Board function 
+        ship[i][j]=0;//From ship functtion
+        for (int i=0;i<9;i++)
+        {
+            for (int j=0;j<9;j++)
+            {
+                if (ship[i][j]==1)
+                {
+                    Board[i][j]=ship[i][j];
+                }
+                else if (ship[i][j]==0)
+                {
+                    Board[i][j]=0;
+                }
+            }
+        }
+        for (int i=0;i<9;i++)
+        {
+            for (int j=0;j<9;j++)
+            {
+                i = x;
+                j = y;
+                if(Board[i][j]==1)
+                {
+                    System.out.print ("X");//X mean hit
+                }
+                else
+                {
+                    System.out.print ("*");//* mean miss
+                }
+            }
+        }
+    }
+
+}
