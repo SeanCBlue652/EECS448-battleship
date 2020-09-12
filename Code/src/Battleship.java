beee7317219990f834fc655caac2e560e1863e3c
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2020-09-11 22:51:37 -0500

    Preliminary work on the main game logic

--- Code/src/Battleship.java
+++ Code/src/Battleship.java
@@ -1,10 +1,10 @@
 //If classes come from files in the same folder, you don't have to import them :)
+//You can compile this program in the terminal with "javac Battleship.java" and run it with "java Battleship"
 
 public class Battleship {
     public static void main(String[] args) {
         //This the the program's main
-        executive gameHandler = new executive(); // This is me instantiating an executive class, which comes from the other file in the same folder.
-        gameHandler.helloWorld(); //You can compile this program in the terminal with "javac Battleship.java" and run it with "java Battleship"
-        gameHandler.clearTerminal();
+        executive gameHandler = new executive(); 
+        gameHandler.run();
     }
 }
